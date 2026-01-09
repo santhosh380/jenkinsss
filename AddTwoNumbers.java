3af7443ffc53d79927baab011b18d9f74462a5c8
santhosh380 <mvsanthosh108@gmail.com>
2026-01-09 11:56:48 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two integer variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
